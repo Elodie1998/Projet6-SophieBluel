a1f93685143d9b148c158c44eacd86c50e2c9c2a
Elodie G <elog1998@gmail.com>
2024-12-31 19:12:15 +0100

    Ajout des filtres en js et du css

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/MonCodeJS/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/MonCodeJS/script.js
@@ -1,25 +1,49 @@
 async function recuperationTravaux() { //opé asynchrones (tps), await gère promesses + facilement
     try { //tester code
-        const response = await fetch("http://localhost:5678/api/works");//requête API, attend promesse pr continuer
-        if (!response.ok) {//si réponse API réussie
-            throw new Error("Erreur lors de la récupération des données");//sinon erreur ac message
+        const reponseT = await fetch("http://localhost:5678/api/works");//requête API, attend promesse pr continuer
+        if (!reponseT.ok) {//si réponse API réussie
+            throw new Erreur("Erreur lors de la récupération des données");//sinon erreur ac message
         }
-        const travaux = await response.json(); // Convertion réponse en JSON, accès + facilement données
+        const travaux = await reponseT.json(); // Convertion réponse en JSON, accès + facilement données
         console.log(travaux);
         for (let i = 0; i < travaux.length; i++) {
-            AfficherProjet(travaux[i]);
+            obtenirProjet(travaux[i]);
         }
-    } catch (error) { //si erreur, gère
-        console.error("Il y a eu un problème :", error); // Gère les erreurs
+    } catch (erreur) { //si erreur, gère
+        console.error("Il y a eu un problème :", erreur); // Gère les erreurs
     }
 }
-
 // Appel de la fonction
 recuperationTravaux(); //exécution de la fonction
 
-function AfficherProjet(info) {
+function obtenirProjet(info) {
     const projet = document.createElement("figure");
     projet.innerHTML = `<img src=${info.imageUrl} alt=${info.title}>
-				<figcaption>${info.title}</figcaption>`
+				<figcaption>${info.title}</figcaption>`;
     document.querySelector(".gallery").appendChild(projet);
+}
+
+async function obtenirCategories() {
+    try {
+        const reponseC = await fetch("http://localhost:5678/api/categories");
+        if (!reponseC.ok) {
+            throw new Erreur("Erreur lors de la récupération des données");
+        }
+
+        const categories = await reponseC.json();
+        console.log(categories);
+        for (let i = 0; i < categories.length; i++) {
+            afficherFiltres(categories[i]);
+        }
+    } catch (erreur) {
+        console.error("Il y a eu un problème :", erreur);
+    }
+}
+obtenirCategories();
+
+
+function afficherFiltres(info) {
+    const divConteneur = document.createElement("div");
+    divConteneur.innerHTML = `${info.name}`;
+    document.querySelector(".div-conteneur").appendChild(divConteneur);
 }
